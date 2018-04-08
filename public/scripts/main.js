4ede4775877589b08047098f22163495c508657e
Vivek Vijay <vivek.vijay@litmus7.com>
2018-04-08 17:46:38 +0530

    changes

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,64 +1,13 @@
 (() => {
-    const xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function () {
-        if (xmlhttp.readyState == XMLHttpRequest.DONE) {
-            if (xmlhttp.status == 200) {
-                start(JSON.parse(xmlhttp.responseText));
-            } else {
-                alert('Some error occurred while retrieving config.');
-            }
-        }
-    };
-    xmlhttp.open("GET", "/config", true);
-    xmlhttp.send();
+
+    let dataAccess = dataAccessModule,
+        domManipulator = domManipulationModule;
+
+    dataAccess.getInitialConfig(domManipulator.getStart);
+
+    document.querySelector('.start-button').addEventListener('click',()=>{
+        // domManipulator.resetGame();
+        dataAccess.getResult(domManipulator.startSpin);        
+    })
+
 })();
-
-
-function start(config) {
-
-    let columnCount = config.columns,
-        imageCount = config.images.length,
-        e = 0,        
-        container = document.getElementsByClassName('container')[0],
-        img, child, prev, beforePrev, blocks;
-
-
-    for(let a=0;a<columnCount;a++){
-
-        child = container.appendChild(document.createElement("div"));
-        child.className="child";
-
-        for(let i=0;i<imageCount;i++){
-           img= child.appendChild(document.createElement("img"));
-           img.src=config.images[i];
-        }
-
-    }
-
-    blocks = document.getElementsByClassName('child');
-
-    setInterval(function () {
-        e++;
-        if (e > imageCount) {
-            e = 1;
-        }
-        prev = e - 1, beforePrev = e - 2;
-
-        switch (prev) {
-            case 0:
-                prev = imageCount;
-                beforePrev = imageCount - 1;
-                break;
-            case 1:
-                beforePrev = imageCount;
-        }
-
-        for(let j=0;j<columnCount;j++){
-            blocks[j].children[e - 1].className = 'current';
-            blocks[j].children[prev - 1].className = 'previous';
-            blocks[j].children[beforePrev - 1].className = 'before-prev';
-        }
-        
-
-    }, 200);
-}
