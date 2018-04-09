9687231f6e4c2bbde6a66ff2fae4753ae605df52
Vivek Vijay <vivek.vijay@litmus7.com>
2018-04-09 11:37:00 +0530

    Changed spinning direction

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -3,8 +3,10 @@
     let dataAccess = dataAccessModule,
         domManipulator = domManipulationModule;
 
+    //Loading initial setups.
     dataAccess.getInitialConfig(domManipulator.getStart);
 
+    //Binding Button click
     document.querySelector('.start-button').addEventListener('click',()=>{
         domManipulator.resetGame();
         dataAccess.getResult(domManipulator.startSpin);        
